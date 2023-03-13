484e039dcb9effb58d4a9e97d2fbfa7168006cb0
Viktoriia-Lehusova <vika01059953@gmail.com>
2023-03-14 00:07:51 +0200

    rewrite searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
@@ -10,54 +10,48 @@ import {
   StyledInput,
 } from './Searchbar.styled';
 
-class Searchbar extends Component {
-  state = {
-    imageValue: '',
+export default function Searchbar({ onSubmit }) {
+  const [imageValue, setImageValue] = useState('');
+
+  const handleSearchValueChange = evt => {
+    setImageValue(evt.currentTarget.value.toLowerCase());
   };
 
-  handleSearchValueChange = evt => {
-    this.setState({ imageValue: evt.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
-    if (this.state.imageValue.trim() === '') {
+    if (imageValue.trim() === '') {
       toast.error('Something went wrong.');
       return;
     }
 
-    this.props.onSubmit(this.state.imageValue);
-    this.setState({ imageValue: '' });
+    onSubmit(imageValue);
+    setImageValue('');
   };
 
-  render() {
-    return (
-      <StyledHeader className="searchbar">
-        <StyledForm className="form" onSubmit={this.handleSubmit}>
-          <StyledBtn type="submit" className="button">
-            <StyledSpan className="button-label">Search</StyledSpan>
-            <BiSearchAlt2 />
-          </StyledBtn>
+  return (
+    <StyledHeader className="searchbar">
+      <StyledForm className="form" onSubmit={handleSubmit}>
+        <StyledBtn type="submit" className="button">
+          <StyledSpan className="button-label">Search</StyledSpan>
+          <BiSearchAlt2 />
+        </StyledBtn>
 
-          <StyledInput
-            className="input"
-            type="text"
-            name="imageValue"
-            value={this.state.imageValue}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleSearchValueChange}
-          />
-        </StyledForm>
-      </StyledHeader>
-    );
-  }
+        <StyledInput
+          className="input"
+          type="text"
+          name="imageValue"
+          value={imageValue}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleSearchValueChange}
+        />
+      </StyledForm>
+    </StyledHeader>
+  );
 }
 
 StyledForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-
-export default Searchbar;
